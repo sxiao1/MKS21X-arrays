924fa0d307e395a72768eab100b3a2b0445f9f78
sxiao1 <sxiao1@stuy.edu>
2018-09-26 19:08:30 -0400

    fill2D

--- ArrayDemo.java
+++ ArrayDemo.java
@@ -13,7 +13,7 @@ public class ArrayDemo{
   }
   public static void printArray(int[][]ary){
     for(int x = 0; x < ary.length; x++){
-      for (int y = 0; x < ary[y].length; y++){
+      for (int y = 0; y < ary[x].length; y++){
         if (y == 0){
           System.out.println("[");
           System.out.print(ary[y] + ",");
@@ -25,6 +25,29 @@ public class ArrayDemo{
           System.out.print(ary[y] + ",");
         }
       }
+  }
+}
+public static int countZeros2D(int[][] nums){
+  int count = 0;
+  for(int x = 0; x < nums.length; x++){
+    for (int y = 0; y < nums[x].length; y++){
+      if(nums[x][y] == 0){
+        count ++;
+      }
+    }
+  }
+  return count;
+}
+public static void fill2D(int[][] vals){
+  for(int x = 0; x < vals.length; x++){
+    for (int y = 0; y < vals[x].length; y++){
+      if (x == y){
+        vals[x][y] = 3;
+      }
+      else{
+        vals[x][y] = 1;
+      }
+    }
   }
 }
 
